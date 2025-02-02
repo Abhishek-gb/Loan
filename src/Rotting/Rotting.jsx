c00b2a41ace256fbbbd594a165907688791be73d
Abhishek-gb <gbabhishek6361@gmail.com>
2025-02-02 15:02:58 +0530

    Updated feature X

--- src/Rotting/Rotting.jsx
+++ src/Rotting/Rotting.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate} from 'react-router-dom';
 import ContactUs from '../Components/ContactUs';
 import LoanServices from '../Components/LoanServices';
 import AboutUs from '../Components/AboutUs';
